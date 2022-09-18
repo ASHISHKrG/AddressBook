2f5f629dc0a1e016f7aa87e597208b2c254b526b
Ashish <aashish7322@gmail.com>
2022-09-18 21:26:42 +0530

    package com.bridgelabz.addressBook;

--- src/main/java/com/bridgelabz/addressBook/AddressBook.java
+++ src/main/java/com/bridgelabz/addressBook/AddressBook.java
@@ -1,6 +1,8 @@
 package com.bridgelabz.addressBook;
 
-class Contacts {
+import java.util.Scanner;
+
+class Contact {
 
 	String FirstName;
 	String LastName;
@@ -8,20 +10,20 @@ class Contacts {
 	String City;
 	String State;
 	String PhoneNO;
-	int ZipCode;
 	String Email;
+	int ZipCode;
 
-	public Contacts(String firstName, String address, String city, String state, String phoneNO, int zipCode,
-			String email) {
-		super();
-		FirstName = firstName;
-		Address = address;
-		City = city;
-		State = state;
-		PhoneNO = phoneNO;
-		ZipCode = zipCode;
-		Email = email;
-	}
+//	public Contact(String firstName, String address, String city, String state, String phoneNO, int zipCode,
+//			String email) {
+//		super();
+//		FirstName = firstName;
+//		Address = address;
+//		City = city;
+//		State = state;
+//		PhoneNO = phoneNO;
+//		ZipCode = zipCode;
+//		Email = email;
+//	}
 
 	void showContacts() {
 		System.out.println("FirstName : " + FirstName + "\nAddress: " + Address + " \nEmail : " + Email);
@@ -33,8 +35,29 @@ public class AddressBook {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Address Book");
 
-		Contacts contact = new Contacts("Ashish", "Burlington", "Lucknow", "UP", "9044855917", 226001,
-				"aashish7322@gmail.com");
+		// Contact contact= new
+		// Contact("Ashish","Burlington","Lucknow","UP","9044855917",226001,"aashish7322@gmail.com");
+		Contact contact = new Contact();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the FirstName");
+		contact.FirstName = sc.nextLine();
+		System.out.println("Enter the LastName");
+		contact.LastName = sc.nextLine();
+		System.out.println("Enter the Address");
+		contact.Address = sc.nextLine();
+		System.out.println("Enter the City");
+		contact.City = sc.nextLine();
+		System.out.println("Enter the State");
+		contact.State = sc.nextLine();
+		System.out.println("Enter the Phone NO");
+		contact.PhoneNO = sc.nextLine();
+		System.out.println("Enter the Email");
+		contact.Email = sc.nextLine();
+		System.out.println("Enter the ZipCode");
+		contact.ZipCode = sc.nextInt();
+		
+
 		contact.showContacts();
+		
 	}
 }
